50b6a548133ac8cb292931cc16cf7aaee343cd81
Wes <wesonweb@gmail.com>
2018-09-20 22:39:21 +0100

    tidied up code

--- page-about.php
+++ page-about.php
@@ -1,22 +1,11 @@
-<?php
-/*
-Template Name: About
-*/
-?>
+<?php /* Template Name: About */ ?>
 
 <?php get_header(); ?>
-
 	<div class="container">
-
-				<h2 class="page-title">	<?php the_title() ?></h2>
-
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-						<?php the_content( ); ?>
-
+		<h2 class="page-title">	<?php the_title() ?></h2>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php the_content( ); ?>
 		         	<?php endwhile; endif;  ?>
 
-  </div>
-	<!-- end inner-wrap div -->
-
+	  </div>
 <?php get_footer(); ?>
